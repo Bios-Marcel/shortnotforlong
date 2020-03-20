25ddded356d4b904680dd489759368d40ee47bdf
Avahe Kellenberger <avahe@protonmail.ch>
2020-03-20 14:19:46 -0400

    Added file-type extensions for shortened URLs. (#2)
    * Added filetype extensions for shortened URLs.
    
    * Enhanced regex, added additional test.

--- tests/shortnotforlong_test.go
+++ tests/shortnotforlong_test.go
@@ -1,11 +1,14 @@
 package tests
 
 import (
-	"net/http"
-	"testing"
-
 	linkshortener "github.com/Bios-Marcel/shortnotforlong"
+	"net/http"
+	"regexp"
+	"strings"
+	"testing"
 )
+
+// URLs without a colon prefix (e.g. http://) are not supported by the go http library
 
 func TestShortener(t *testing.T) {
 	shortener := linkshortener.NewShortener(55555)
@@ -39,4 +42,60 @@ func TestShortener(t *testing.T) {
 			t.Errorf("URL was incorrect: %s", response.Request.URL.String())
 		}
 	}
+
+	{
+		suffix := ".png"
+		URL := "https://duckduckgo.com/img" + suffix
+		shortenedURL := shortener.Shorten(URL)
+		response, httpError := http.Head(shortenedURL)
+		if httpError != nil {
+			t.Fatalf("Error shortening looking up: '%s' (%s). Error: %s", shortenedURL, URL, httpError.Error())
+		}
+
+		if response.Request.URL.String() != URL {
+			t.Errorf("URL was incorrect: %s", response.Request.URL.String())
+		}
+
+		if !strings.HasSuffix(shortenedURL, suffix) {
+			t.Errorf("Shortened url does not end with: %s", suffix)
+		}
+	}
+
+	{
+		suffix := ".webm"
+		URL := "http://duckduckgo.com/vid" + suffix
+		shortenedURL := shortener.Shorten(URL)
+		response, httpError := http.Head(shortenedURL)
+		if httpError != nil {
+			t.Fatalf("Error shortening looking up: '%s' (%s). Error: %s", shortenedURL, URL, httpError.Error())
+		}
+
+		if response.Request.URL.String() != URL {
+			t.Errorf("URL was incorrect: %s", response.Request.URL.String())
+		}
+
+		if !strings.HasSuffix(shortenedURL, suffix) {
+			t.Errorf("Shortened url does not end with: %s", suffix)
+		}
+	}
+
+	{
+		URL := "https://github.com"
+		shortenedURL := shortener.Shorten(URL)
+		response, httpError := http.Head(shortenedURL)
+		if httpError != nil {
+			t.Fatalf("Error shortening looking up: '%s' (%s). Error: %s", shortenedURL, URL, httpError.Error())
+		}
+
+		if response.Request.URL.String() != URL {
+			t.Errorf("URL was incorrect: %s", response.Request.URL.String())
+		}
+
+		var urlSuffix = regexp.MustCompile(`.*(/\d+)$`)
+		var matches = urlSuffix.FindStringSubmatch(shortenedURL)
+		if len(matches) == 0 {
+			t.Errorf("Shortened url does not end with '/{number}' - %s", shortenedURL)
+		}
+	}
+
 }
